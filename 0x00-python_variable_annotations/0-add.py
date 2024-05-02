1296822d6aec5a060f4ef492c98650ad4c3a9396
hailegnaw27 <ixvos406@gmail.com>
2024-05-02 15:45:43 -0700

    advanced

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+This module contains the add function.
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Takes two floats, a and b, and returns their sum as a float.
+
+    :param a: First float.
+    :param b: Second float.
+    :return: The sum of a and b.
+    """
+    return a + b
+
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+This module contains the concat function.
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Takes two strings, str1 and str2, and returns their concatenated result.
+
+    :param str1: First string.
+    :param str2: Second string.
+    :return: The concatenated string of str1 and str2.
+    """
+    return str1 + str2
+
